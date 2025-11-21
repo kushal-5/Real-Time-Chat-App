34b8862865b30c90496869870815b0ff8a6eb6e9
kushal-5 <prohackie3@gmail.com>
2025-11-21 22:57:20 +0545

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,12 +19,16 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "20mb" }));           // Accept bigger payloads
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+
+// Only use CORS in dev
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
+}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
