b4b2b8c4e0e4b73e7a0393fd3801c09a4dfc74c2
kushal-5 <prohackie3@gmail.com>
2025-11-21 15:22:27 +0545

    frontend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));           // Accept bigger payloads
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
